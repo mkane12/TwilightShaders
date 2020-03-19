a89a268791fe363302dc4e53056a57b72a3982ad
Michaela <michaela.j.kane@gmail.com>
2020-03-19 17:12:16 +0900

    defined initial color values in script

---
+++ Assets/Scripts/twilight_twocolor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class twilight_twocolor : MonoBehaviour
+{
+    // pass colors to shader for a two-color gradient
+    public Material gradient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gradient.SetColor("_Color0", new Color32(255, 157, 0, 1));
+        gradient.SetColor("_Color1", new Color32(25, 53, 193, 1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
