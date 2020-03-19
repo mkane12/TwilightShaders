51389c185f3cb78107d57a63f28dac966e7a595a
Michaela <michaela.j.kane@gmail.com>
2020-03-19 17:16:42 +0900

    made color variables public and available in editor

--- Assets/Scripts/twilight_twocolor.cs
+++ Assets/Scripts/twilight_twocolor.cs
@@ -6,12 +6,14 @@ public class twilight_twocolor : MonoBehaviour
 {
     // pass colors to shader for a two-color gradient
     public Material gradient;
+    public Color32 bottomColor = new Color32(255, 157, 0, 1);
+    public Color32 topColor = new Color32(25, 53, 193, 1);
 
     // Start is called before the first frame update
     void Start()
     {
-        gradient.SetColor("_Color0", new Color32(255, 157, 0, 1));
-        gradient.SetColor("_Color1", new Color32(25, 53, 193, 1));
+        gradient.SetColor("_Color0", bottomColor);
+        gradient.SetColor("_Color1", topColor);
     }
 
     // Update is called once per frame
