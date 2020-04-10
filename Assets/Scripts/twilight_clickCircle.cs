40d9ef4878a8322bddc814f104a701dad076c925
Michaela <michaela.j.kane@gmail.com>
2020-04-10 20:40:53 +0900

    circle shader that moves with click complete

---
+++ Assets/Scripts/twilight_clickCircle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class twilight_clickCircle : MonoBehaviour
+{
+    // pass colors to shader for a two-color gradient
+    public Material gradient;
+    public Color32 middleColor = new Color32(255, 157, 0, 1);
+    public Color32 outerColor = new Color32(25, 53, 193, 1);
+    // position of user click
+    public Vector3 clickPos = new Vector3(0.5f, 0.5f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set initial colors
+        gradient.SetColor("_Color0", middleColor);
+        gradient.SetColor("_Color1", outerColor);
+
+        // determine screen coordinates of plane object just once, since it's not moving
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // OnValidate() called when script is loaded or value cahnged in the Inspector
+    void OnValidate()
+    {
+        // ensure any changes made during runtime are reflected
+        gradient.SetColor("_Color0", middleColor);
+        gradient.SetColor("_Color1", outerColor);
+    }
+
+    // called on click
+    void OnMouseDown()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast (ray, out hit))
+        {
+            Debug.Log(hit.point);
+            // This plane is scaled evenly on x and z axes, but in case it's not in the future
+
+            // screen coordinates range from (-25, -25) on lower left to (25, 25) on upper right.
+            // We start by making it all positive
+            clickPos = new Vector3(hit.point.x + this.transform.localScale.x * this.transform.localScale.z, 
+                hit.point.y + this.transform.localScale.x * this.transform.localScale.z, 
+                clickPos.z);
+            // next, we need to scale so it ranges from (0, 0) to (1, 1), instead of (0, 0) to (50, 50)
+            clickPos.x = clickPos.x / (this.transform.localScale.x * this.transform.localScale.z * 2);
+            clickPos.y = clickPos.y / (this.transform.localScale.x * this.transform.localScale.z * 2);
+
+            // only change center if user clicked plane
+            Debug.Log(clickPos);
+            gradient.SetVector("_Pos", clickPos);
+        }
+        
+    }
+}
---
+++ Assets/Shaders/Twilight-clickCircle.shader
@@ -0,0 +1,54 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Custom/Twilight-clickCircle"
+{
+	// Gradient shader between 2 colors with clicked position
+	Properties
+    {
+        _Color0 ("Middle Color", Color) = (1,1,1,1)
+		_Color1 ("Outer Color", Color) = (1,1,1,1)
+		_Pos ("Click Position", Vector) = (0.5, 0.5, 0, 0)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+ 
+			#include "UnityCG.cginc"
+ 
+			struct appdata_t {
+				float4 vertex : POSITION;
+				float2 texcoord : TEXCOORD0;
+			};
+ 
+			struct v2f {
+				float4 vertex : SV_POSITION;
+				half2 texcoord : TEXCOORD0;
+			};
+ 
+			v2f vert (appdata_t v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.texcoord = v.texcoord;
+				return o;
+			}
+ 
+			fixed4 _Color0, _Color1;
+			float2 _Pos;
+ 
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// this sure required a lot of futzing around heck
+				float t = length(i.texcoord - float2(_Pos.y, 1-_Pos.x)) * 1.41421356237; // 1.141... = sqrt(2)
+				return lerp(_Color0, _Color1, t);
+			}
+			ENDCG
+         }
+     }
+ }
